bc14ed017e4fdd75af0723a0475857fd1f0223ed
martinmoec <martin.moe.carstens@itpartner.no>
2023-04-26 00:17:02 +0200

    Update Interop.fs

--- src/Interop.fs
+++ src/Interop.fs
@@ -1,3 +1,18 @@
 namespace Feliz.Native
 
 open Fable.Core
+open Fable.Core.JsInterop
+open Fable.React
+open Feliz.ReactApi
+
+/// Describes an element property
+type IReactProperty = interface end
+
+/// Describes style attribute
+type IStyleAttribute = interface end
+
+/// Describes an SVG attribute
+type ISvgAttribute = interface end 
+
+type ReactElement = Fable.React.ReactElement
+type IRefValue<'T> = Fable.React.IRefValue<'T>
